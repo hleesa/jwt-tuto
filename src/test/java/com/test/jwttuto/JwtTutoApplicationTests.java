5b878df4eaa54f8210aef323ab3a56f8778d7b3a
salee2 <lshhi100@gmail.com>
2023-01-18 16:03:35 +0900

    Initial commit

---
+++ src/test/java/com/test/jwttuto/JwtTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.jwttuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
